1683c348e00c80c07f9034e209698dce518a613e
EtienneAbeng <abengetienne@gmail.com>
2024-05-17 14:42:46 +0200

    Write a script that gets the contents of a webpage and stores it in a file.
    The first argument is the URL to request
    The second argument the file path to store the body response
    The file must be UTF-8 encoded
    You must use the module request

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
-// Importation du module 'request' pour effectuer des requêtes HTTP
+// Importer le module 'request' pour effectuer des requêtes HTTP
 const request = require('request');
-// Importation du module 'fs' pour manipuler les fichiers
+// Importer le module 'fs' pour la gestion des fichiers
 const fs = require('fs');
 
-// Récupération de l'URL de la page à partir des arguments de la ligne de commande
-const url = process.argv[2];
-// Récupération du chemin du fichier où enregistrer le contenu à partir des arguments de la ligne de commande
-const filePath = process.argv[3];
+// Récupérer l'URL à partir des arguments de ligne de commande
+const urlToRequest = process.argv[2];
+// Récupérer le chemin du fichier où enregistrer la réponse à partir des arguments de ligne de commande
+const filePathToStore = process.argv[3];
 
 // Effectuer une requête à l'URL spécifiée
-request(url, (error, response, body) => {
-  // Vérifier s'il y a une erreur lors de la requête
+request(urlToRequest, (error, response, body) => {
+  // Vérifier s'il y a eu une erreur lors de la requête
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+    // Écrire le contenu de la réponse dans le fichier spécifié
+    fs.writeFile(filePathToStore, body, 'utf8', (error) => {
+      // Gérer les erreurs lors de l'écriture dans le fichier
+      if (error) {
+        console.log(error);
+      }
+    });
   }
-
-  // Écrire le contenu de la réponse dans le fichier spécifié
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    // Vérifier s'il y a une erreur lors de l'écriture du fichier
-    if (err) {
-      console.error(err);
-    }
-  });
 });
