baa74d3114196dea1cb8d22c5db34af2336f1bdf
EtienneAbeng <abengetienne@gmail.com>
2024-05-17 10:40:22 +0200

    Write a script that prints the number of movies where the character “Wedge Antilles” is present.
    The first argument is the API URL of the Star wars API: https://swapi-api.hbtn.io/api/films/
    Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
    You must use the module request

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+// Importation du module 'request' pour effectuer des requêtes HTTP
+const request = require('request');
+
+// Récupération de l'URL de l'API Star Wars à partir des arguments de ligne de commande
+const apiUrl = process.argv[2];
+
+// ID du personnage "Wedge Antilles"
+const characterId = 18;
+
+// Effectuer une requête à l'API Star Wars pour obtenir les détails des films
+request(apiUrl, (error, response, body) => {
+    // Vérifier si une erreur est survenue
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    // Analyser la réponse JSON
+    const data = JSON.parse(body);
+
+    // Initialiser le compteur de films
+    let count = 0;
+
+    // Parcourir les films
+    for (const film of data.results) {
+        // Vérifier si le casting contient l'URL du personnage "Wedge Antilles"
+        for (const characterUrl of film.characters) {
+            if (characterUrl.includes(characterId)) {
+                // Incrémenter le compteur de films
+                count++;
+                break; // Sortir de la boucle interne une fois que le personnage est trouvé
+            }
+        }
+    }
+
+
+    // Afficher le nombre de films dans lesquels "Wedge Antilles" apparaît
+    console.log(`Wedge Antilles appears in ${count} films`);
+})
